c082561bb4d0862b70f36504983886f36811c396
nipun <nipunchawlanc@gmail.com>
2025-08-12 03:13:19 +0530

    Pixel Art

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-const result=document.querySelector(".result");
-var yourscore=0,computerscore=0;
-function playRound(human,computer){
-    if((human==="Scissors" &&computer==="Paper") || (human==="Paper"&&computer==="Rock") || (human==="Rock"&&computer==="Scissors")){
-        result.textContent=`You won`;
-        yourscore++;
-    }else if((human==="Scissors" &&computer==="Rock") || (human==="Paper"&&computer==="Scissors") || (human==="Rock"&&computer==="Paper")){
-        result.textContent=`You lost`;
-        computerscore++;
-    }else{result.textContent=`Draw`};
+
+const container = document.querySelector(".container");
+const buttons = document.querySelectorAll("#generate");
+const num = document.querySelector("#num");
+const drawingColorInput = document.querySelector("#drawcolor");
+let drawingColor = "#000000";
+
+document.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+function makeGrid(size) {
+    container.innerHTML = "";
+    if (size < 10 || size > 100) {
+        alert("Please enter a number between 10 and 100.");
+        return;
+    }
+    for (let i = 0; i < size; i++) {
+        let column = document.createElement("div");
+        column.classList.add("column");
+        for (let j = 0; j < size; j++) {
+            let row = document.createElement("div");
+            row.addEventListener('mousedown', (event) => {
+                isDrawing = true;
+                event.target.style.transition = 'background-color 0.2s';
+                event.target.style.backgroundColor = drawingColor;
+            });
+            row.addEventListener('mouseover', (event) => {
+                if (isDrawing) {
+                    event.target.style.transition = 'background-color 0.2s';
+                    event.target.style.backgroundColor = drawingColor;
+                }
+            });
+            row.classList.add("row");
+            column.appendChild(row);
+        }
+        container.appendChild(column);
+    }
 }
-const buttons=document.querySelectorAll("button");
+makeGrid(+(num.value));
+buttons.forEach(button => {
 
-const userOption=document.querySelector(".userOption");
-const compOption=document.querySelector(".compOption");
-const scoreBoard=document.querySelector(".score")
-buttons.forEach((button)=>{
-    button.addEventListener("click",()=>{
-    const op=Math.floor(Math.random()*3);
-    var choice;
-    if(op===0){choice="Rock"}else if(op===1){choice="Paper"}else{choice="Scissors"};
-    userOption.textContent=`The option you selected is : ${button.textContent}`;
-    compOption.textContent=`The option computer selected is: ${choice}`;
-    playRound(button.textContent,choice);
-    scoreBoard.textContent=`Your Score : ${yourscore} Computer's Score : ${computerscore}`;
+    button.addEventListener("click", () => {
+        const size = +num.value; makeGrid(size)
+    });
+
 });
-});
-
-
-
